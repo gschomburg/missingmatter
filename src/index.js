31a7b506cd9337f4e1055a162fd75507c7fc992f
gschomburg <gschomburg@gmail.com>
2017-08-30 17:04:15 -0400

    trying webpack

---
+++ src/index.js
@@ -0,0 +1,16 @@
+//require('/main.scss');
+
+/*
+import _ from 'lodash';
+
+function component() {
+  var element = document.createElement('div');
+
+  // Lodash, currently included via a script, is required for this line to work
+  element.innerHTML = _.join(['Hellxxxxo', 'webpack'], ' ');
+
+  return element;
+}
+
+document.body.appendChild(component());
+*/
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+var path = require('path');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: ['./src/index.js', './src/main.scss'],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    port: 8080
+  },
+  module: {
+    loaders: [
+        {
+          test: /\.(sass|scss)$/,
+          loader: ExtractTextPlugin.extract(['css-loader', 'sass-loader'])
+        }
+    ]
+  },
+  plugins: [
+       new ExtractTextPlugin('main.css', {
+           allChunks: true
+       }),
+       new CopyWebpackPlugin([
+            { from: './src/images' , to: './images'}
+        ])
+   ]
+}
